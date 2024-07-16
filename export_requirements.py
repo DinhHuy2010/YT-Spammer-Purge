08aeff52270053ff07655ff6d349d8dc3e77b81f
DinhHuy2010 <huy04052010@outlook.com>
2024-07-16 13:47:28 +0700

    fix export_requirements.py

--- export_requirements.py
+++ export_requirements.py
@@ -1,21 +1,28 @@
 import subprocess
 import sys
 
-COMMAMD = [
-    sys.executable,
-    "poetry",
-    "export",
-    "--without-hashes",
-    "--without-urls",
-    "--output=requirements.txt",
-]
+import shutil
+
 
 def main() -> int:
+    poetry = shutil.which("poetry")
+    if poetry is None:
+        print("Error: poetry not found")
+        return 1
+    COMMAMD = [
+        poetry,
+        "export",
+        "--without-hashes",
+        "--without-urls",
+        "--output=requirements.txt",
+    ]
     response = subprocess.run(COMMAMD, check=False, capture_output=True)
     if response.returncode != 0:
-        print("failed")
+        print("failed:")
+        print(response.stderr)
         return 1
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
